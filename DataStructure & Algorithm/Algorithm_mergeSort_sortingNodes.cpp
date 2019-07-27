a392721e1eca09d19dbdf43fd70ca860c2d0ba76
gun0317 <39793048+gun0317@users.noreply.github.com>
2019-07-27 11:16:26 +0900

    upgraded the code

---
+++ DataStructure & Algorithm/Algorithm_mergeSort_sortingNodes.cpp
@@ -0,0 +1,59 @@
+#define MAX_NODES 500000
+#include <iostream>
+using namespace std;
+
+typedef struct grade {
+	// score: bigger the better, rating: smaller the better. **rating comes first**
+	int score, rating;
+}grade;
+
+int compare(grade &a, grade &b) {
+	if (a.rating != b.rating) return -(a.rating - b.rating);	// the smaller the better
+	return a.score - b.score;	// the bigger the better
+}
+
+grade sorted[MAX_NODES];
+void merge(grade* list, int left, int right) {
+	int mid = (left + right) / 2;
+	int first = left, second = mid + 1, idx = left;
+	while (left <= mid && second <= right)
+		if (compare(list[first], list[second]) > 0) sorted[idx++] = list[first++];
+		else sorted[idx++] = list[second++];
+	if (first <= mid)
+		for (int i = first; i <= mid; i++) sorted[idx++] = list[i];
+	if (second <= right)
+		for (int i = second; i <= right; i++) sorted[idx++] = list[i];
+	for (int i = left; i <= right; i++) list[i] = sorted[i];
+}
+
+void mergeSort(grade* list, int left, int right) {
+	if (left < right) {
+		int mid = (left + right) / 2;
+		mergeSort(list, left, mid);
+		mergeSort(list, mid + 1, right);
+		merge(list, left, right);
+	}
+}
+
+void printList(grade* arr) {
+	for (int i = 0; i < 5; i++) {
+		printf("arr[%d] - rating: %d, score: %d\n", i, arr[i].rating, arr[i].score);
+	}
+}
+
+int main() {
+	grade arr[5];
+	arr[0].rating = 5, arr[0].score = 32;
+	arr[1].rating = 3, arr[1].score = 56;
+	arr[2].rating = 1, arr[2].score = 99;
+	arr[3].rating = 3, arr[3].score = 52;
+	arr[4].rating = 1, arr[4].score = 100;
+	printList(arr);
+	mergeSort(arr, 0, 4);
+	cout << "SORTED" << endl;
+	printList(arr);
+
+
+
+	return 0;
+}
---
+++ DataStructure & Algorithm/Algorithm_stringHashTable_del.cpp
@@ -0,0 +1,117 @@
+﻿#define MAX_NODES 50
+#define MAX_BUCKET 2
+#include <iostream>
+using namespace std;
+
+struct hashNode {
+	char str[20];
+	hashNode* nextNode;
+	hashNode* prevNode;
+	hashNode() {
+		nextNode = 0, prevNode = 0;
+	}
+}static_alloc_nodes[MAX_NODES];
+
+int static_alloc_idx = 0;
+hashNode* static_alloc() {
+	return &static_alloc_nodes[static_alloc_idx++];
+}
+
+void myStrcpy(char* dest, const char* src) {
+	for (; *src != '\0'; *dest++ = *src++);
+}
+
+int myStrcmp(const char* s1, const char* s2) {
+	while (*s2 != '\0') {
+		if (*s1 > *s2) return 1;
+		else if (*s1 < *s2) return -1;
+		s1++, s2++;
+	}
+	if (*s1 != '\0') return 1;
+	else return 0;
+}
+
+struct strHashTable {
+private:
+	hashNode* bucket[MAX_BUCKET];
+
+	int hash(const char* item) {
+		unsigned long h = 5381;
+		while (*item != '\0')
+			h = ((h >> 5) + h + *item++) % MAX_BUCKET;
+		return (int)h;
+	}
+	hashNode* findBucket(const char* item) {
+		int h = hash(item);
+		if (bucket[h] == 0) return 0;
+		else
+			return bucket[h];
+	}
+
+public:
+	void init() {
+		for (int i = 0; i < MAX_BUCKET; i++) bucket[i] = 0;
+	}
+
+	void push(const char* item) {
+		int h = hash(item);
+		hashNode* toInsert = static_alloc();
+		myStrcpy(toInsert->str, item);
+		hashNode* cur = bucket[h];
+		if (cur == 0) {
+			bucket[h] = toInsert;
+			toInsert->prevNode = 0;
+			return;
+		}
+		else {
+			bucket[h]->prevNode = toInsert;
+			toInsert->nextNode = bucket[h];
+			bucket[h] = toInsert;
+		}
+		cout << item << " added" << endl;
+	}
+
+	int find(const char* item) {
+		int h = hash(item);
+		if (bucket[h] == 0)return 0;
+		hashNode* cur = bucket[h];
+		while (cur != 0) {
+			if (myStrcmp(cur->str, item) == 0) return 1;
+			cur = cur->nextNode;
+		}
+		return 0;
+	}
+
+	void del(const char* item) {
+		hashNode* cur = findBucket(item);
+		if (cur == 0) return;
+		while (cur != 0) {
+			if (myStrcmp(cur->str, item) == 0) break;
+			cur = cur->nextNode;
+		}
+		if (myStrcmp(cur->str, item) != 0) return;
+
+		hashNode* prev = cur->prevNode;
+		hashNode* next = cur->nextNode;
+		if (prev != 0) prev->nextNode = next;
+		if (next != 0) next->prevNode = prev;
+		cout << item << " deleted" << endl;
+	}
+};
+
+int main() {
+	char s1[] = "abc", s2[] = "abcd", s3[] = "123", s4[] = "fvdkcljx", s5[] = "-1231";
+
+	strHashTable hT;
+	hT.init();
+	hT.push(s1);
+	hT.push(s2);
+	hT.push(s3);
+	hT.push(s4);
+	hT.push(s5);
+	printf("finding s1,2,3,4,5: %d %d %d %d %d\n", hT.find(s1), hT.find(s2), hT.find(s3), hT.find(s4), hT.find(s5));
+	hT.del(s3);
+	printf("finding s1,2,3,4,5: %d %d %d %d %d\n", hT.find(s1), hT.find(s2), hT.find(s3), hT.find(s4), hT.find(s5));
+
+	return 0;
+}
