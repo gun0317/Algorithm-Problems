2f733028b456706d0a18a4d7fec508aee1477a99
이영건 <ygun.lee@navercorp.com>
2021-05-09 01:30:41 +0900

    format

--- BaekJoon/2503 숫자야구.cpp
+++ BaekJoon/2503 숫자야구.cpp
@@ -1,67 +1,67 @@
-//#include <iostream>
-//
-//using namespace std;
-//int base[3], candidates[1000];
-//int N;
-//int temp, s, b;
-//
-//void divide(int * arr, int input) {
-//
-//	arr[0] = input / 100;
-//	input -= arr[0] * 100;
-//	arr[1] = input / 10;
-//	input -= arr[1] * 10;
-//	arr[2] = input;
-//}
-//
-//bool isNotAns(int candidate) {
-//	int candi[3],localS=0,localB=0;
-//	divide(candi,candidate);
-//	for(int i=0;i<3;i++)
-//		for (int j = 0; j < 3; j++) {
-//			if (base[i] == candi[j]) {
-//				if (i == j)
-//					localS++;
-//				else
-//					localB++;
-//			}
-//		}
-//	if (localS == s && localB == b)
-//		return 0;
-//	return 1;
-//}
-//
-//int main() {
-//	for (int i = 123; i < 988; i++) {
-//		candidates[i] = i;
-//		int cand[3];
-//		divide(cand, i);
-//		if (cand[0] == cand[1] || cand[1] == cand[2] || cand[0] == cand[2] || cand[0] == 0 || cand[1] == 0 || cand[2] == 0)
-//			candidates[i] = 0;
-//	}
-//
-//	 cin >> N;
-//	for (int i = 0; i < N; i++) {
-//		
-//		cin >> temp >> s>> b;
-//		
-//		//Á¶°ÇÀ» ÇÑÀÚ¸®¾¿ ¶âÀ½
-//		divide(base, temp);
-//
-//		for (int j = 123; j < 988; j++) {
-//			if (candidates[j] != 0)
-//			{
-//				if (isNotAns(candidates[j]))
-//					candidates[j] = 0;
-//			}
-//		}
-//	}
-//	int ans = 0;
-//	for (int i = 123; i < 988; i++)
-//		if (candidates[i] != 0)
-//			ans++;
-//	cout << ans << endl;
-//
-//
-//	return 0;
-//}
+#include <iostream>
+
+using namespace std;
+int base[3], candidates[1000];
+int N;
+int temp, s, b;
+
+void divide(int * arr, int input) {
+
+	arr[0] = input / 100;
+	input -= arr[0] * 100;
+	arr[1] = input / 10;
+	input -= arr[1] * 10;
+	arr[2] = input;
+}
+
+bool isNotAns(int candidate) {
+	int candi[3],localS=0,localB=0;
+	divide(candi,candidate);
+	for(int i=0;i<3;i++)
+		for (int j = 0; j < 3; j++) {
+			if (base[i] == candi[j]) {
+				if (i == j)
+					localS++;
+				else
+					localB++;
+			}
+		}
+	if (localS == s && localB == b)
+		return 0;
+	return 1;
+}
+
+int main() {
+	for (int i = 123; i < 988; i++) {
+		candidates[i] = i;
+		int cand[3];
+		divide(cand, i);
+		if (cand[0] == cand[1] || cand[1] == cand[2] || cand[0] == cand[2] || cand[0] == 0 || cand[1] == 0 || cand[2] == 0)
+			candidates[i] = 0;
+	}
+
+	 cin >> N;
+	for (int i = 0; i < N; i++) {
+		
+		cin >> temp >> s>> b;
+		
+		//ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ú¸ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
+		divide(base, temp);
+
+		for (int j = 123; j < 988; j++) {
+			if (candidates[j] != 0)
+			{
+				if (isNotAns(candidates[j]))
+					candidates[j] = 0;
+			}
+		}
+	}
+	int ans = 0;
+	for (int i = 123; i < 988; i++)
+		if (candidates[i] != 0)
+			ans++;
+	cout << ans << endl;
+
+
+	return 0;
+}
--- BaekJoon/2583 영역 구하기(RE).cpp
+++ BaekJoon/2583 영역 구하기(RE).cpp
@@ -1,51 +1,51 @@
-//#include <iostream>
-//#include <algorithm>
-//#include <vector>
-//#include <functional>
-//#include <stack>
-//#include <queue>
-//#include <string.h> // test caseº° memset»ç¿ë
-//#define pair pair <int,int>
-//#define boundary newR>=0&&newC>=0&&newR<M&&newC<N
-//using namespace std;
-//int dr[] = { -1,1,0,0 };
-//int dc[] = { 0,0,-1,1 };
-//int N, M, K, map[101][101], visited[101][101], area[101];
-//
-//void DFS(pair cur,int idx) {
-//	visited[cur.first][cur.second] = 1;
-//	for (int i = 0; i < 4; i++) {
-//		int newR = cur.first + dr[i];
-//		int newC = cur.second + dc[i];
-//		if (boundary && !map[newR][newC] && !visited[newR][newC])
-//			DFS(make_pair(newR, newC),idx);
-//	}
-//	area[idx]++;
-//}
-//
-//int main() {
-//	cin >> M >> N >> K;
-//	int lr, lc, rr, rc;
-//	for (int i = 0; i < K; i++) {
-//		cin >> lc >> lr >> rc >> rr;
-//		for (int r = lr; r < rr; r++)
-//			for (int c = lc; c < rc; c++)
-//				map[r][c] = 1;
-//	}
-//	
-//	int idx = 0, components = 0;
-//	for (int r = 0; r < M; r++)
-//		for (int c = 0; c < N; c++) {
-//			if (!visited[r][c] && !map[r][c]) {
-//				DFS(make_pair(r, c), idx++);
-//				components++;
-//			}
-//		}
-//	sort(area, area + components);
-//	cout << components << endl;
-//	for (int i = 0; i < components; i++) {
-//		cout << area[i] << " ";
-//	}
-//
-//	return 0;
-//}
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <string.h> // test caseï¿½ï¿½ memsetï¿½ï¿½ï¿½
+#define pair pair <int,int>
+#define boundary newR>=0&&newC>=0&&newR<M&&newC<N
+using namespace std;
+int dr[] = { -1,1,0,0 };
+int dc[] = { 0,0,-1,1 };
+int N, M, K, map[101][101], visited[101][101], area[101];
+
+void DFS(pair cur,int idx) {
+	visited[cur.first][cur.second] = 1;
+	for (int i = 0; i < 4; i++) {
+		int newR = cur.first + dr[i];
+		int newC = cur.second + dc[i];
+		if (boundary && !map[newR][newC] && !visited[newR][newC])
+			DFS(make_pair(newR, newC),idx);
+	}
+	area[idx]++;
+}
+
+int main() {
+	cin >> M >> N >> K;
+	int lr, lc, rr, rc;
+	for (int i = 0; i < K; i++) {
+		cin >> lc >> lr >> rc >> rr;
+		for (int r = lr; r < rr; r++)
+			for (int c = lc; c < rc; c++)
+				map[r][c] = 1;
+	}
+	
+	int idx = 0, components = 0;
+	for (int r = 0; r < M; r++)
+		for (int c = 0; c < N; c++) {
+			if (!visited[r][c] && !map[r][c]) {
+				DFS(make_pair(r, c), idx++);
+				components++;
+			}
+		}
+	sort(area, area + components);
+	cout << components << endl;
+	for (int i = 0; i < components; i++) {
+		cout << area[i] << " ";
+	}
+
+	return 0;
+}
